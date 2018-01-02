285af6b864eec4be34c02a6b7338a891c11a6e3b
roman <roman@telichk.in>
2018-01-02 23:26:31 +0300

    Add cheatsheet as erlang source file

--- cheatsheet/supervisor_cheatsheet.erl
+++ cheatsheet/supervisor_cheatsheet.erl
@@ -1,19 +1,3 @@
-# Learn Erlang
-
-Learn Erlang with ["Learn you some Erlang from great good!"](http://learnyousomeerlang.com)
-
-
-## Useful chapters
-
-- [records explain](http://learnyousomeerlang.com/a-short-visit-to-common-data-structures#records)
-- [spawn_link explain](http://learnyousomeerlang.com/errors-and-processes#links)
-- [trap_exit explain](http://learnyousomeerlang.com/errors-and-processes#its-a-trap)
-- [monitors explain](http://learnyousomeerlang.com/errors-and-processes#monitors)
-
-
-## OTP CheatSheet
-
-```erlang
 -module(supervisor_cheatsheet).
 -behaviour(supervisor).
 
@@ -175,4 +159,3 @@ child_spec(Restart, Shutdown) ->
   Modules = [ChildModule],
 
   {ChildId, Start, Restart, Shutdown, ChildType, Modules}.
-```  
